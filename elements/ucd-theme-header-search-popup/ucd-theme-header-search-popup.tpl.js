a808df556f36c21952174e066f2c5cd7e3958653
spelkey-ucd <spelkey@ucdavis.edu>
2021-05-10 15:38:53 -0400

    search pop over

---
+++ elements/ucd-theme-header-search-popup/ucd-theme-header-search-popup.tpl.js
@@ -0,0 +1,30 @@
+import { html, css } from 'lit';
+
+import formStyles from "@ucd-lib/theme-sass/1_base_html/_forms.css.js";
+import popupStyles from "@ucd-lib/theme-sass/4_component/_search-popup.css.js";
+
+export function styles() {
+  const elementStyles = css`
+    :host {
+      display: block;
+    }
+  `;
+
+  return [
+    formStyles,
+    popupStyles,
+    elementStyles];
+}
+
+export function render() { 
+return html`
+<button 
+  class="search-popup__open ${this.opened ? 'search-popup__open--close' : ''}" 
+  @click=${this._onBtnClick}>
+  <span class="search-popup__open-icon">${this.buttonText}</span>
+</button>
+<div class="search-popup ${this.opened ? 'is-open' : ''}">
+  <slot></slot>
+</div>
+
+`;}
--- test-app/index.js
+++ test-app/index.js
@@ -2,3 +2,4 @@ import '../elements/ucd-theme-alert/ucd-theme-alert';
 import '../elements/ucd-theme-message-area/ucd-theme-message-area';
 import "../elements/ucd-theme-list-accordion/ucd-theme-list-accordion";
 import "../elements/ucd-theme-form-search/ucd-theme-form-search";
+import "../elements/ucd-theme-header-search-popup/ucd-theme-header-search-popup";
