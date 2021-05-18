53b1b84e59fe2cb06f4c902ea5e302977c3e6e35
spelkey-ucd <spelkey@ucdavis.edu>
2021-05-18 14:40:32 -0400

    document gallery component

--- elements/ucd-theme-image-gallery/ucd-theme-image-gallery.js
+++ elements/ucd-theme-image-gallery/ucd-theme-image-gallery.js
@@ -4,6 +4,50 @@ import {render, styles} from "./ucd-theme-image-gallery.tpl.js";
 import PhotoSwipe from 'photoswipe';
 import PhotoSwipeUI from 'photoswipe/dist/photoswipe-ui-default';
 
+/**
+ * @class UcdThemeImageGallery
+ * @classdesc Component class for displaying an image gallery.
+ * Any direct img children will be displayed in the gallery.
+ *  PatternLab Url:
+ *    - http://dev.webstyleguide.ucdavis.edu/redesign/?p=molecules-image-gallery
+ *    - http://dev.webstyleguide.ucdavis.edu/redesign/?p=molecules-image-gallery-poster
+ * 
+ * @property {String} modifier - any class modifier(s) ... e.g. 'poster'
+ * 
+ * @example
+ * // multiple images. If no 'big-src' is passed, the lightbox will display the src image
+ * // width and height are required
+ * html`
+ *  <ucd-theme-image-gallery>
+ *    <img 
+ *      src="http://placehold.it/352x198" 
+ *      alt="16x9 Image" 
+ *      width="352" 
+ *      height="198" 
+ *      big-src="http://placehold.it/2000x1125" 
+ *      big-width="2000" 
+ *      big-height="1125" />
+ *    <img 
+ *      src="http://placehold.it/352x198" 
+ *      width="352"
+ *      height="198" />
+ *  </ucd-theme-image-gallery>
+ * `
+ * // A single 'poster' image
+ * html`
+ * <ucd-theme-image-gallery modifier="poster">
+ *    <img 
+ *      src="http://placehold.it/352x198" 
+ *      alt="16x9 Image" 
+ *      width="352" 
+ *      height="198" 
+ *      big-src="http://placehold.it/2000x1125" 
+ *      big-width="2000" 
+ *      big-height="1125" />
+ *    <img 
+ * </ucd-theme-image-gallery>
+ * `
+ */
 export default class UcdThemeImageGallery extends LitElement {
 
   static get properties() {
@@ -64,6 +108,13 @@ export default class UcdThemeImageGallery extends LitElement {
         height: this._getImgAttr(child, 'height'),
       };
 
+      for (const attr of ['width', 'height']) {
+        if ( !bigImage[attr] ) {
+          console.warn(`Skipping image. Missing '${attr}' attribute`);
+          return;
+        }
+      }
+
       images.push({
         child,
         title,
@@ -74,22 +125,53 @@ export default class UcdThemeImageGallery extends LitElement {
     });
 
     this._images = images;
-    console.log(images);
   }
 
+  /**
+   * @method _onClick
+   * @description Bound to  each <a> tag surrounding image slots
+   * @param {Event} e Click event
+   */
   _onClick(e){
     e.preventDefault();
-    console.log(e);
     this.openLightBox(e.target.getAttribute('image-index'));
-
-
   }
 
+  /**
+   * @method _dispatchOpenEvent
+   * @description Fires 'open-lightbox' custom event.
+   * @param {Number} index - Index of focused image.
+   */
+  _dispatchOpenEvent(index){
+    let e = new CustomEvent('open-lightbox', {
+      detail: { 
+        message: 'Lightbox has been opened for a gallery image', 
+        imageIndex: index,
+        image: this._images[index]
+      },
+      bubbles: true,
+      composed: true });
+    this.dispatchEvent(e);
+  }
+
+  /**
+   * @method _getImgAttr
+   * @description Utility class for extracting an attribute from child image
+   * @param {Object} ele - DOM reference to an img child
+   * @param {*} attr - An element attribute
+   * @returns {String}
+   */
   _getImgAttr(ele, attr){
     return ele.getAttribute(`big-${attr}`) ? ele.getAttribute(`big-${attr}`) : ele.getAttribute(attr);
   }
 
-  openLightBox(index=0){
+  /**
+   * @method openLightBox
+   * @description Opens a lightbox for the specified gallery image.
+   * @param {Number} index - Image index
+   * @param {Boolean} dispatchEvent - Whether to fire 'open-lightbox' event
+   */
+  openLightBox(index=0, dispatchEvent=false){
     const dialog = this.shadowRoot.getElementById('light-box');
     const items = this._images.map(image => {
       return {
@@ -114,9 +196,9 @@ export default class UcdThemeImageGallery extends LitElement {
       }
     };
 
-    console.log(dialog, items, options);
     const lightBox = new PhotoSwipe(dialog, PhotoSwipeUI, items, options);
     lightBox.init();
+    if ( dispatchEvent ) this._dispatchOpenEvent(index);
   }
 
   /**
