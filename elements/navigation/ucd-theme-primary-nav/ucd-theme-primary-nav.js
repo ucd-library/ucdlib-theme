de3f078a7aef05ca5bc155a66d9d605f394cd6d7
spelkey-ucd <spelkey@ucdavis.edu>
2021-07-09 17:53:36 -0400

    checking in

--- elements/navigation/ucd-theme-primary-nav/ucd-theme-primary-nav.js
+++ elements/navigation/ucd-theme-primary-nav/ucd-theme-primary-nav.js
@@ -1,5 +1,6 @@
 import { LitElement, html } from 'lit';
 import {render, styles} from "./ucd-theme-primary-nav.tpl.js";
+import { styleMap } from 'lit/directives/style-map.js';
 
 import Mixin from "../../utils/mixin";
 import { MutationObserverElement } from "../../utils/mutation-observer";
@@ -12,15 +13,18 @@ import { MutationObserverElement } from "../../utils/mutation-observer";
  *  - http://dev.webstyleguide.ucdavis.edu/redesign/patterns/molecules-navigation-00-primary-nav-megamenu/molecules-navigation-00-primary-nav-megamenu.rendered.html
  * 
  * @property {String} styleModifier - Apply an alternate style with a keyword:
+ *  'superfish' - The default
  *  'mega' - Hovering over any top-level link opens a single nav with all subnav links
  * @property {Number} hoverDelay - How long (ms) after hover will menu open/close
+ * @property {Number} animationDuration - How long (ms) for a menu to fade in/out
+ * @property {Number} maxDepth - Maximum number of submenus to show
  * 
  * @example
  * html`
  *  <ucd-theme-primary-nav>
  *    <a href="#">link 1</a>
  *    <a href="#">link 2</a>
- *    <ul name="link with subnav" href="#">
+ *    <ul link-title="link with subnav" href="#">
  *      <li><a href="#">subnav link 1</a></li>
  *    </ul>
  *  </ucd-theme-primary-nav>
@@ -33,6 +37,7 @@ export default class UcdThemePrimaryNav extends Mixin(LitElement)
     return {
       styleModifier: {type: String, attribute: "style-modifier"},
       hoverDelay: {type: Number, attribute: "hover-delay"},
+      animationDuration: {type: Number, attribute: "animation-duration"},
       navItems: {type: Array},
       maxDepth: {type: Number, attribute: "max-depth"}
     };
@@ -45,8 +50,9 @@ export default class UcdThemePrimaryNav extends Mixin(LitElement)
   constructor() {
     super();
     this.render = render.bind(this);
-    this.styleModifier = "";
+    this.styleModifier = "superfish";
     this.hoverDelay = 300;
+    this.animationDuration = 300;
     this.navItems = [];
     this.maxDepth = 2;
 
@@ -72,7 +78,7 @@ export default class UcdThemePrimaryNav extends Mixin(LitElement)
    * @returns {Object} Formatted object describing the menu item and its children
    */
   _makeNavItemTree(ele){
-    let linkText, href, subItems = [];
+    let linkText, href, subItems = [], isOpen=false, mobileStyles={};
     if ( ele.tagName === 'LI' && ele.children.length > 0) ele = ele.children[0];
 
     if ( ele.tagName === 'A' ) {
@@ -89,7 +95,7 @@ export default class UcdThemePrimaryNav extends Mixin(LitElement)
     }
 
     if ( linkText ) linkText = linkText.trim();
-    return {linkText, href, subItems};
+    return {linkText, href, subItems, isOpen, mobileStyles};
 
   }
 
@@ -97,26 +103,39 @@ export default class UcdThemePrimaryNav extends Mixin(LitElement)
    * @method _renderNavItem
    * @description Renders a menu item and all its children to the specified max depth
    * @param {Object} navItem - An item from the 'navItems' element property
-   * @param {Number} depth - The current depth within the menu hierarchy
+   * @param {Array} location - Coordinates of the item in the 'navItems' array. i.e. [0, 1, 4]
    * @returns {TemplateResult}
    */
-  _renderNavItem(navItem, depth=0){
+  _renderNavItem(navItem, location){
+    const depth = location.length - 1;
+
+    // Render item and its subnav
     if ( this._hasSubNav(navItem) && depth < this.maxDepth) {
       return html`
-      <li>
+      <li 
+        .key=${location}
+        @mouseenter=${this._onItemMouseenter} 
+        @mouseleave=${this._onItemMouseleave}
+        class="${navItem.isOpen ? 'sf--hover' : ''} ${navItem.isClosing ? 'closing': ''}">
         <div class="submenu-toggle__wrapper ${depth === 0 ? `${this._classPrefix}__top-link` : ''}">
           <a href=${navItem.href}>${navItem.linkText}<span class="${this._classPrefix}__submenu-indicator"></span></a>
-          <button class="submenu-toggle" aria-label="Toggle Submenu"><span class="submenu-toggle__icon"></span></button>
+          <button 
+          @click=${(e) => this._toggleMobileMenu(e.target, location)}
+          class="submenu-toggle ${navItem.isOpen ? 'submenu-toggle--open' : ''}" 
+          aria-label="Toggle Submenu">
+          <span class="submenu-toggle__icon"></span>
+        </button>
         </div>
-        <ul class="menu">
-          ${navItem.subItems.map(subItem => this._renderNavItem(subItem, depth + 1))}
+        <ul class="menu" style=${styleMap(this.getItemMobileStyles(location))}>
+          ${navItem.subItems.map((subItem, i) => this._renderNavItem(subItem, location.concat([i])))}
         </ul>
       </li>
     `;
     }
 
+    // render as normal link
     return html`
-      <li class="${depth === 0 ? `${this._classPrefix}__top-link`: '' }">
+      <li .key=${location} class="${depth === 0 ? `${this._classPrefix}__top-link`: '' }">
         ${navItem.href ? html`
           <a href=${navItem.href}>${navItem.linkText}</a>
         ` : html`
@@ -124,7 +143,104 @@ export default class UcdThemePrimaryNav extends Mixin(LitElement)
         `}
       </li>
     `;
+  }
 
+  // this is a disaster
+  async _toggleMobileMenu(ele, navLocation){
+    if ( window.innerWidth >= 992 ) return;
+    let ul = ele.parentElement.nextElementSibling;
+    if ( !ul || ul.tagName !== 'UL' ) return;
+    let navItem = this.getNavItem(navLocation);
+    
+    // collapse menu
+    if ( navItem.isOpen ) {
+      navItem.mobileStyles.height = ul.scrollHeight + "px";
+      navItem.mobileStyles.display = "block";
+      navItem.mobileStyles.transition = "height 300ms";
+      this.requestUpdate();
+      await this.updateComplete;
+      navItem.mobileStyles.height = "0px";
+      this.requestUpdate();
+      await this.updateComplete;
+      let onClose = () => {
+        console.log("hello");
+        ul.removeEventListener('transitionend', onClose);
+        navItem.mobileStyles = {};
+        navItem.isOpen = !navItem.isOpen;
+        this.requestUpdate();
+      };
+      ul.addEventListener('transitionend', onClose);
+
+
+    // expand menu
+    } else {
+      navItem.mobileStyles.display = "block";
+      navItem.mobileStyles.height = 0 + "px";
+      this.requestUpdate();
+      await this.updateComplete;
+      navItem.mobileStyles.height = ul.scrollHeight + "px";
+      let onOpen = () => {
+        ul.removeEventListener('transitionend', onOpen);
+        //if (navItem.mobileStyles.height) delete navItem.mobileStyles.height;
+        navItem.isOpen = !navItem.isOpen;
+        this.requestUpdate();
+      };
+      ul.addEventListener('transitionend', onOpen);
+
+      
+
+    }
+    this.requestUpdate();
+
+  }
+
+  /**
+   * @method _onItemMouseenter
+   * @description Bound to nav li items with a subnav
+   * @param {Event} e 
+   */
+  _onItemMouseenter(e){
+    if ( window.innerWidth < 992 ) return;
+    let navLocation = e.target.key;
+    let navItem = this.getNavItem(navLocation);
+    this.clearMobileStyles(navItem);
+    if ( navItem.isClosing ) {
+      navItem.isClosing = false;
+      this.requestUpdate();
+    }
+    if ( navItem.timeout ) clearTimeout(navItem.timeout);
+    if ( navItem.isOpen ) return;
+
+    
+    navItem.timeout = setTimeout(() => {
+      navItem.isOpen = true;
+      this.requestUpdate();
+    }, this.hoverDelay);
+    
+    
+  }
+
+  /**
+   * @method _onItemMouseleave
+   * @description Bound to nav li items with a subnav
+   * @param {Event} e 
+   * @returns 
+   */
+  _onItemMouseleave(e){
+    if ( window.innerWidth < 992 ) return;
+    let navLocation = e.target.key;
+    let navItem = this.getNavItem(navLocation);
+    this.clearMobileStyles(navItem);
+    if ( navItem.timeout ) clearTimeout(navItem.timeout);
+    if ( !navItem.isOpen ) return;
+
+    navItem.isClosing = true;
+    this.requestUpdate();
+    navItem.timeout = setTimeout(() => {
+      navItem.isOpen = false;
+      navItem.isClosing = false;
+      this.requestUpdate();
+    }, this.hoverDelay + this.animationDuration);
   }
 
   /**
@@ -136,6 +252,34 @@ export default class UcdThemePrimaryNav extends Mixin(LitElement)
   _hasSubNav(navItem){
     if ( navItem && navItem.subItems && navItem.subItems.length) return true;
     return false;
+  }
+
+  /**
+   * @method getNavItem
+   * @description Retrieves an item from the navItems array.
+   * @param {Array} location - Coordinates of the item in the 'navItems' array. i.e. [0, 1, 4].
+   * @returns {Object}
+   */
+  getNavItem(location){
+    let accessor = "this.navItems";
+    if ( location && location.length > 0) {
+      accessor += "[" + location.join("].subItems[") + "]";
+    }
+    return eval(accessor);
+  }
+
+  getItemMobileStyles(location) {
+    if ( window.innerWidth >= 992 ) return {};
+    let navItem = this.getNavItem(location);
+    if ( !navItem.mobileStyles ) return {};
+    return navItem.mobileStyles;
+  }
+
+  clearMobileStyles(navItem){
+    if (navItem.mobileStyles && Object.keys(navItem.mobileStyles).length > 0 ) {
+      navItem.mobileStyles = {};
+      this.requestUpdate();
+    }
   }
 
 }
--- elements/navigation/ucd-theme-primary-nav/ucd-theme-primary-nav.tpl.js
+++ elements/navigation/ucd-theme-primary-nav/ucd-theme-primary-nav.tpl.js
@@ -1,30 +1,74 @@
 import { html, css } from 'lit';
 
-import menuStyles from "@ucd-lib/theme-sass/2_base_class/_misc.css.js"
+import normalizeStyles from "@ucd-lib/theme-sass/normalize.css.js";
+import formStyles from "@ucd-lib/theme-sass/1_base_html/_forms.css.js";
+import menuStyles from "@ucd-lib/theme-sass/2_base_class/_misc.css.js";
 import primaryNavStyles from "@ucd-lib/theme-sass/4_component/_nav-primary.css.js";
+import subNavToggleStyles from "@ucd-lib/theme-sass/4_component/_submenu-toggle.css.js";
 
 export function styles() {
   const elementStyles = css`
     :host {
       display: block;
     }
+    .submenu-toggle * {
+      pointer-events: none;
+    }
+    @media (min-width: 992px) {
+      ul.menu ul.menu {
+        opacity: 0;
+      }
+      ul.menu li.sf--hover > ul.menu {
+        display: block;
+        opacity: 1;
+      }
+      ul.menu li.closing > ul.menu {
+        display: block;
+        opacity: 0;
+      }
+    }
+
+    @media (max-width: 991px) {
+      ul.menu ul.menu {
+        display: none;
+        overflow-y: hidden;
+        visibility: visible;
+        height: auto;
+        border-top-width: 0px;
+        border-bottom-width: 0px;
+        padding-top: 0px;
+        padding-bottom: 0px;
+      }
+    }
   `;
 
   return [
+    normalizeStyles,
+    formStyles,
     menuStyles,
     primaryNavStyles,
+    subNavToggleStyles,
     elementStyles
   ];
 }
 
 export function render() { 
 return html`
+<style>
+  ul.menu ul.menu {
+    transition: opacity ${this.animationDuration + "ms"}, height ${this.animationDuration + "ms"};
+  }
+  ul.menu li.sf--hover > ul.menu {
+    transition: opacity ${this.animationDuration + "ms"} ${this.hoverDelay + "ms"}, height ${this.animationDuration + "ms"};
+  }
+
+</style>
 <nav 
   id=${this._classPrefix}
-  class="${this._classPrefix} ${this._classPrefix}--superfish ${this.styleModifier ? `${this._classPrefix}--${this.styleModifier}` : ""}" 
+  class="${this._classPrefix} ${this.styleModifier ? `${this._classPrefix}--${this.styleModifier}` : ""}" 
   aria-label="Main Menu">
   <ul class="menu">
-    ${this.navItems.map(navItem => this._renderNavItem(navItem))}
+    ${this.navItems.map((navItem, i) => this._renderNavItem(navItem, [i]))}
   </ul>
 </nav>
 `;}
