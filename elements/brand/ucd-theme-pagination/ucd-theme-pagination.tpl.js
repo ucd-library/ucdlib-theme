2b88cbc1f251610a93cbd0a7324e7159326ebf8f
Sabrina Baggett <sbagg@Sabrina-MacBook-Pro.local>
2021-09-02 14:20:50 -0700

    pagination and ucdlib-academic icons

--- elements/brand/ucd-theme-pagination/ucd-theme-pagination.tpl.js
+++ elements/brand/ucd-theme-pagination/ucd-theme-pagination.tpl.js
@@ -9,12 +9,29 @@ export function styles() {
     :host {
       display: block;
     }
+    .default {
+      display: flex;
+    }
+    .xs-screen {
+      display: none;
+    }
     @media (min-width: 991px) {
       .pager__item--next {
         display: flex;
         justify-content: center;
         align-items: center;
       }
+    }
+    @media (max-width: 992px) {
+      .default {
+        display: none;
+      }
+      .xs-screen {
+        display: flex;
+        justify-content: center;
+        align-items: center
+      }
+    }
   `;
 
   return [normalizeCss, resetCss, paginationCss, elementStyles];
@@ -24,28 +41,35 @@ export function render() {
 return html`
 
   <ul class="pager">
-    ${this._renderLink(
-      this.currentPage-1, 
-      {label: 'Prev', class: 'pager__item--previous', noHighlight: false}
-    )}
-    
-    ${this._pages.map(page => this._renderLink(page))}
+    <div class="xs-screen">
+      ${this._renderLink(
+        this.currentPage-1, 
+        {label: 'Prev', class: 'pager__item--previous', noHighlight: false}
+      )}
 
-    ${this.xs_screen ? 
-      html`
-        <span style="display: flex;justify-content: center;align-items: center;">of ${this.maxPages}</span>  
+      ${this._renderLink(this.currentPage)}
+      <span>of ${this.maxPages}</span>  
         ${this._renderLink(
           this.currentPage+1, 
           {label: 'Next', class: 'pager__item--next', noHighlight: false}
         )} 
-      `: 
-      html `
-        ${this._renderLink(
-          this.currentPage+1, 
-          {label: 'Next', class: 'pager__item--next', noHighlight: false}
-        )} 
-      `
-    }
+    </div>
+
+    <div class="default">
+      ${this._renderLink(
+        this.currentPage-1, 
+        {label: 'Prev', class: 'pager__item--previous', noHighlight: false}
+      )}
+    
+      ${this._pages.map(page => this._renderLink(page))}
+
+      ${this.xs_screen ? html`<span class="xs-screen" style="display: flex;justify-content: center;align-items: center;">of ${this.maxPages}</span>`:``}
+
+      ${this._renderLink(
+        this.currentPage+1, 
+        {label: 'Next', class: 'pager__item--next', noHighlight: false}
+      )} 
+    </div>
   </ul>
 
 `;}
