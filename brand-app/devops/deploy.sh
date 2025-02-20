0f33692ec2ecacbfc21d483f81eb85136a971eda
Justin Merz <jrmerz@gmail.com>
2025-02-20 10:43:45 -0800

    adding cork-kube build integration

---
+++ brand-app/devops/deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -e
+
+TAG=${$1:-main}
+REGISTRY=us-west1-docker.pkg.dev/digital-ucdavis-edu/pub
+IMAGE=$REGISTRY/ucdlib-theme-app:$TAG
+
+gcloud beta run deploy ucdlib-theme-app \
+  --image=$IMAGE \
+  --platform=managed \
+  --memory=1Gi \
+  --region=us-central1
