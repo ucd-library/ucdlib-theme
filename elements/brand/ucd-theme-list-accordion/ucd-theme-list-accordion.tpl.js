4f09894a01be6e69acf05cf7ca9984b56eeaac08
spelkey-ucd <spelkey@ucdavis.edu>
2025-06-03 09:50:14 -0400

    #110

--- elements/brand/ucd-theme-list-accordion/ucd-theme-list-accordion.tpl.js
+++ elements/brand/ucd-theme-list-accordion/ucd-theme-list-accordion.tpl.js
@@ -18,32 +18,32 @@ export function styles() {
     }
   `;
   return [
-    listCss, 
+    listCss,
     customStyles
   ]
 }
 
-export function render() { 
+export function render() {
 return html`
   <ul class="list--${this.listStyle}">
   ${this._listItems.map((item, index) => html`
     ${this._isTitle(index) ? html`
-      <li 
+      <li
         id="accordion-${index}"
         class="item-title ${this.itemIsExpanded(index, false) ? 'active' : ''}"
-        item-index="${index}" 
+        item-index="${index}"
         tabindex="0"
         @click=${this._onTitleClick}
         @keyup=${this._onTitleKeyUp}
-        aria-controls="accordion-${index}-panel"
+        aria-controls="accordion-${index + 1}-panel"
         aria-expanded="${this.itemIsExpanded(index, false)}">
         <slot name="${item.slotName}"></slot>
       </li>
     ` : html`
-      <li 
-        id="accordion-${index}-panel" 
+      <li
+        id="accordion-${index}-panel"
         class="item-content"
-        aria-labelledby="accordion-${index}" 
+        aria-labelledby="accordion-${index - 1}"
         ?hidden="${!this.itemIsExpanded(index, false)}">
         <slot name="${item.slotName}"></slot>
       </li>
